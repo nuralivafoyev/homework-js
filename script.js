04ace52b2aeec55e56e6ee0ec523c8572314cb16
nuralivafoyev <nurikboy555@gmail.com>
2024-04-01 12:35:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+// 1-masala
+// k va n butun son (n>0), k sonni n marta chiqarilsin
+// let k = +prompt("Kerakli sonni kiriting: ");
+// let n = +prompt("Nechi marta chiqarish kerak ?");
+// for(let i=1; i<=n; i++){
+//     console.log(k)
+// }
+// console.log("Marxamat yaxshi kunlaga buyursin)");
+
+
+  
+
+
+//2-masala
+//a dan b gacha sonlarni chiqarish
+// let a = +prompt("a ni kiritng:");
+// let b = +prompt("b ni kiritng:");
+// let i = 0;
+// for(i ; a<=b; i++){  
+//     console.log(a);  
+//     a++;
+// }
+// console.log(i + " ta son bor");
+
+
+
+
+
+// 3-masala
+//b dan a gacha sonlarni chiqaradi
+// let a = +prompt("a ni kiritng:");
+// let b = +prompt("b ni kiritng:");
+// for(let i=b - 1; i > a; i--){ 
+//     console.log(i)
+// }
+
+
+
+
+
+// 4-masala
+// konfet narxi 10 kgagacha
+// let sum=+prompt("Konfet narxini kiriting: ");
+// for(let i=1; i<=10; i++){
+//     console.log(`${i} kg konfet ${i*sum} so'm`)
+// }
+
+
+
+//5-masala
+// konfet narxi 0.1 kgdan dan 1 kgagacha
+// let sum=+prompt("Konfet narxini kiriting: ");
+// for(let i=1; i<=10; i++){
+//     console.log(`${i/10} kg konfet ${i/10*sum} so'm`)
+// }
+
+
+
+
+
+// 6-masala
+//1.1 kgdan 2 kggacha
+// let sum=+prompt("Konfet narxini kiriting: ");
+// for(let i=10; i<=20; i++){
+//     console.log(`${i/10} kg konfet ${i/10*sum} so'm`)
+// }
+
+
+
+
+//7-masala
+// arifmetik progressiya
+// let a = +prompt("a ni kiriting: ")
+// let b = +prompt("b ni kiriting: ")
+// let sum = 0;
+// for(let i=a; b>i; i++){
+//     sum+=i;
+// }
+// console.log(`${a} dan ${b} gacha raqamlar yigindisi = ${sum}`)
+
+
+
+
+
+// 8-masala
+//a dan b gacha sonlar kopaytmasi
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let sum = 1;
+// for(let i=a; b>i; i++){
+//     sum*=i;
+// }
+// console.log(`${a} dan ${b} gacha raqamlar yigindisi = ${sum}`)
+
+
+
+
+
+//9-masala
+// a dan b gacha sonlar kvadratlari yigindisi
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let sum = 0;
+// for(let i=a; b>i; i++){
+//     sum = sum + i*i;
+// }
+// console.log(`${a} dan ${b} gacha raqamlar yigindisi = ${sum}`)
+
+
+
+
+
+//10-msasla
+//n gacha sonlarni maxrajga qoyib yigindini topish
+// let n=+prompt("Sonni kiriting: ");
+// let sum = 1;
+// for(let i=2; i<=n; i++){
+//     sum = sum + 1/i;
+// }
+// alert(`Natija: ${sum}`);
